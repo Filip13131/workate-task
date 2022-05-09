fbf61e6e81159fb114c5bdd23b69bc62e4bc3c9e
filip13131 <f.kaminski2000@gmail.com>
2022-05-09 13:41:31 +0200

    fix #1

--- src/components/Photos/Guide/Guide.js
+++ src/components/Photos/Guide/Guide.js
@@ -1,10 +1,11 @@
 import "./Guide.css"
 import ProgressBar from 'react-bootstrap/ProgressBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Guide = ({pageCounter, numberOfPages}) => {
     return (
         <div className="Guide">
-            <ProgressBar now={(pageCounter+1)*100/numberOfPages} />
+            <ProgressBar striped variant="success" active='true' now={(pageCounter+1)*100/numberOfPages} />
             <div>S</div>       
         </div>
     )
